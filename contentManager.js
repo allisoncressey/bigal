93dcd3d3a62e9a39c6993571bc217f9c26518f94
unknown <ryan.b.robinson@gmail.com>
2013-07-01 19:28:38 -0700

    added class for configurations. cleaned up Flickr call

--- contentManager.js
+++ contentManager.js
@@ -4,14 +4,27 @@ var contentArrays = (function(){
 		tumblrImages:[],
 		tumblrVideos:[],
 		oneOffResources:[
-		"<!-- EXAMPLE -- put html of individual images, vids, swfs, etc. -->",
-		"<!-- separate each string with a comma. no comma after the last one -->"
+        {source : "<!-- EXAMPLE -- put html of individual images, vids, swfs, etc. -->", putOnLine : 3, putInAsNumber : 1},
+		{source : "<!-- separate each string with a comma. no comma after the last one -->", putOnLine : 3, putInAsNumber : 1 },
 		],
-		heightOfImages:"330px",
+		heightOfImages:"330px"
 	}
 })();
 
+var contentConfig = (function(){
+    return {
+        randomizeTumblr : false,    // true or false
+        randomizeFlicr: false,      // true or false
+        numberOfRows: 3,           // number, no quotes
+        itemsPerRow : 10,           // number, no quotes
+        putFlickrOnLine : 3,        // number, cannot be higher than numberOfRows
+        putTumblrOnLine : 2,        // number, cannot be higher than numberOfRows
+    }
+})();
+
 var contentManager = (function(){
+
+    var rowItems = [];
 	var flickr = {
 		key: "562c55176e6e55c5bb6200df52bf10da",
 		secret: "1fb761ed3016b0d5",
@@ -28,14 +41,8 @@ var contentManager = (function(){
 		$.ajax({
 		    dataType: 'jsonp',
 		    url: this_url,
-		     jsonp: "callback",
-		     success: function (data) {
-		    	//photoData = data;
-
-		    	//compilePhotoDivs();
-
-		  },
-	  });
+		    jsonp: "callback"
+	    });
 	},
 	getTumblrPhotos = function(cb){
 		$.ajax({
@@ -83,39 +90,21 @@ var contentManager = (function(){
 		  	},
 	  	});
 	}
+    getOneOffItems = function(cb){
+        for (var i = 0; i < contentArrays.oneOffResources.length; i++) {
+        var items = contentArrays.oneOffResources;
+        console.log(items[i].source,items[i].putOnLine);
+
+        if (items[1].putOnLine > contentConfig.numberOfRows){
+            console.log("cannot put resource "+i+" on row "+items[i].putOnLine+". numberOfRows is "+contentConfig.numberOfRows);
+        }
 
 
-	getTumblrPhotos(function(){
-		getTumblrVids(function(){
-			getFlickr()
-		})
-	});
+        }
+        cb();
+    }
 
-	return {
-		processPhotos:function(photoData,cb){
-			console.log(photoData);
-			var body = document.getElementsByTagName('body')[0];
-			for(i=0;i<photoData.photoset.photo.length;i++){
-				var farm = photoData.photoset.photo[i].farm,
-					server = photoData.photoset.photo[i].server,
-					id = photoData.photoset.photo[i].id,
-					secret = photoData.photoset.photo[i].secret,
-					listitems = '<img src="http://farm'+farm+'.staticflickr.com/'+server+'/'+id+'_'+secret+'.jpg" height="'+contentArrays.heightOfImages+'"/>';
-
-				contentArrays.flickrImages.push(listitems);
-
-				if (i == photoData.photoset.photo.length-1){
-    				cb();
-    				return
-    			}
-			}
-		}
-	}
-})();
-
-	// callback for flickr API
-function jsonFlickrApi(data){
- 	contentManager.processPhotos(data,function(){
+    workWithLoadedData = function(){
  		console.log('everything loaded!');
  		var totalArray = contentArrays.tumblrImages.concat(contentArrays.flickrImages).concat(contentArrays.tumblrVideos).concat(contentArrays.oneOffResources);
 
@@ -157,5 +146,60 @@ function jsonFlickrApi(data){
 				mousewheelScrolling: false
 			});
 		}
- 	})
+
+        cb()
+ 	}
+
+    processPhotos = function(photoData,cb){
+        console.log('processPhotos called');
+		console.log(photoData);
+		var body = document.getElementsByTagName('body')[0];
+		for(i=0;i<photoData.photoset.photo.length;i++){
+			var farm = photoData.photoset.photo[i].farm,
+				server = photoData.photoset.photo[i].server,
+				id = photoData.photoset.photo[i].id,
+				secret = photoData.photoset.photo[i].secret,
+				listitems = '<img src="http://farm'+farm+'.staticflickr.com/'+server+'/'+id+'_'+secret+'.jpg" height="'+contentArrays.heightOfImages+'"/>';
+
+			contentArrays.flickrImages.push(listitems);
+
+			if (i == photoData.photoset.photo.length-1){
+    			cb();
+    			return
+    		}
+		}
+	}
+
+    continuePlease = function(){
+        console.log('flickr Finished');
+        getOneOffItems(function(){
+            workWithLoadedData();
+        });
+    }
+
+
+        // starting point for program
+	getTumblrPhotos(function(){
+		getTumblrVids(function(){
+			getFlickr();
+		});
+	});
+
+
+            
+   
+
+	return {
+        flickrCallback : function(data){
+            processPhotos(data,function(){
+                continuePlease()
+            })
+        }
+	}
+})();
+
+	// callback for flickr API
+function jsonFlickrApi(data){
+ 	contentManager.flickrCallback(data);
+    //console.log("jsonp came back");
 }
